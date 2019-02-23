111fc3e7f19ce199386324e184997e3149123e52
Bea Huszár <husz.beata@gmail.com>
2019-02-23 21:13:08 +0100

    add: todo service draft

--- RestTodo/RestTodo/Services/TodoService.cs
+++ RestTodo/RestTodo/Services/TodoService.cs
@@ -1,11 +1,40 @@
-﻿using System;
+﻿using RestTodo.DTOs;
+using RestTodo.Interfaces;
+using RestTodo.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace RestTodo.Services
 {
-    public class TodoService
+    public class TodoService : ICrudService<TodoDto>
     {
+        private readonly ICrudRepository<Todo> repository;
+
+        public void Delete(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TodoDto> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TodoDto GetById(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(TodoDto dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TodoDto dto)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
