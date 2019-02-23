4022caa3849e6e5c5446933ddd32fcd698b0f5ad
Bea Huszár <husz.beata@gmail.com>
2019-02-23 21:13:01 +0100

    add: todo service draft

---
+++ RestTodo/RestTodo/Interfaces/ICrudService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestTodo.Interfaces
+{
+    public interface ICrudService<U> where U : class
+    {
+        IEnumerable<U> GetAll();
+        U GetById(long id);
+        void Save(U dto);
+        void Update(U dto);
+        void Delete(long id);
+    }
+}
---
+++ RestTodo/RestTodo/Services/TodoService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestTodo.Services
+{
+    public class TodoService
+    {
+    }
+}
