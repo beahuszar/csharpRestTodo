8a92523b533f7e79765e755831b23ff0ba28998a
Bea Huszár <husz.beata@gmail.com>
2019-02-23 21:01:23 +0100

    add: todo repo

--- RestTodo/RestTodo/Repositories/TodoRepository.cs
+++ RestTodo/RestTodo/Repositories/TodoRepository.cs
@@ -1,11 +1,46 @@
-﻿using System;
+﻿using RestTodo.Data;
+using RestTodo.Interfaces;
+using RestTodo.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace RestTodo.Repositories
 {
-    public class TodoRepository
+    public class TodoRepository : ICrudRepository<Todo>
     {
+        private readonly CsharpTodoDb context;
+
+        public TodoRepository(CsharpTodoDb context)
+        {
+            this.context = context;
+        }
+
+        public void Delete(long id)
+        {
+            context.Todos.Remove(GetById(id));
+        }
+
+        public IEnumerable<Todo> GetAll()
+        {
+            return context.Todos;
+        }
+
+        public Todo GetById(long id)
+        {
+            return context.Todos.Find(id);
+        }
+
+        public void Save(Todo entity)
+        {
+            context.Todos.Add(entity);
+            context.SaveChanges();
+        }
+
+        public void Update(Todo entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
