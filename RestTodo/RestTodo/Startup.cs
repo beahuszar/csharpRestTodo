82ef8dbb0d3456d990c81c0d791610cdc469387e
Bea Huszár <husz.beata@gmail.com>
2019-02-23 18:30:05 +0100

    refact

--- RestTodo/RestTodo/Startup.cs
+++ RestTodo/RestTodo/Startup.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -30,7 +23,7 @@ namespace RestTodo
         }
 
 
-        public void ConfigureProductionServices(IServiceCollection services)
+        public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
             services.AddDbContext<CsharpTodoDb>(options =>
